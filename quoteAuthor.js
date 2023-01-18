a41a09bd1c7506aceaae1f7daeb2c0acb3240865
Karen <95060071+midas10@users.noreply.github.com>
2023-01-17 21:19:59 -0500

    Update quoteAuthor.js

--- quoteAuthor.js
+++ quoteAuthor.js
@@ -7,12 +7,15 @@
 'use strict';
 
 var initEngineRuntime = 0;
+var posTracker = 0;
 var refreshRate = 10; 		// adjust this var to make refresh rate slower (higher) or faster (slower)
 export function update(value) {
 	thisLayer.text = shared.quoteSettings['quote_author'];
 	thisLayer.origin = thisLayer.origin.subtract(new Vec3(0.2, 0, 0));	// dont change the movement variable for now, it will mess up the ratio w/ the position reset
+	posTracker += 0.2
 	if(engine.runtime - initEngineRuntime > refreshRate){
-		thisLayer.origin = thisLayer.origin.add(new Vec3((refreshRate*9.3), 0, 0))
+		thisLayer.origin = thisLayer.origin.add(new Vec3((posTracker), 0, 0))
+		posTracker = 0;
 		thisLayer.text = shared.quoteSettings['quote_author'];
 		initEngineRuntime = engine.runtime;
 	}
